d9c4688b9c47f2fbfa0abf4ad3f7d8906bd19234
Chris J. Karr <chris@audacious-software.com>
2019-09-03 12:57:38 +0000

    Adding warnining and critical parameters to total process script

--- scripts/django_check_total_processes.py
+++ scripts/django_check_total_processes.py
@@ -5,23 +5,25 @@ import json
 import sys
 import urllib2
 
-CRITICAL = 200
-WARNING = 150
+CRITICAL = 350
+WARNING = 300
 
 parser = argparse.ArgumentParser(description='Checks number of running processes on remote host.')
 
 parser.add_argument('url', help='URL of remote endpoint.')
+parser.add_argument('--critical', help='Number of processes to trigger critical alert.', default=CRITICAL, type=int)
+parser.add_argument('--warning', help='Number of processes to trigger warning alert.', default=WARNING, type=int)
 
 args = parser.parse_args()
 
 response = urllib2.urlopen(args.url) # nosec
-data = json.load(response)   
+data = json.load(response)
 
 if 'count' in data:
-    if data['count'] > CRITICAL:
+    if data['count'] > args.critical:
         print 'PROCS CRITICAL: ' + str(data['count'])
         sys.exit(2)
-    elif data['count'] > WARNING:
+    elif data['count'] > args.warning:
         print 'PROCS WARNING: ' + str(data['count'])
         sys.exit(1)
     else:
