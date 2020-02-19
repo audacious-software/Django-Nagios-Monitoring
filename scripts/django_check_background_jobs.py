501e4b6f6375966a5c830dc1beb475a84aec44f4
Chris J. Karr <chris@audacious-software.com>
2020-02-19 23:55:27 +0000

    Implementing Nagios-side of new background job monitor

---
+++ scripts/django_check_background_jobs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import argparse
+import json
+import sys
+import urllib2
+
+parser = argparse.ArgumentParser(description='Checks that background jobs are running in a timely fashion.')
+
+parser.add_argument('url', help='URL of remote endpoint to check.')
+
+args = parser.parse_args()
+
+response = urllib2.urlopen(args.url) # nosec
+data = json.load(response)
+
+if data['errors']:
+    print 'OVERDUE BACKGROUND JOBS: ' + ','.join(data['errors'])
+    sys.exit(2)
+
+if data['warnings']:
+    print 'OVERDUE BACKGROUND JOBS: ' + ','.join(data['warnings'])
+    sys.exit(1)
+
+print 'BACKGROUND JOBS OK'
+sys.exit(0)
