cb6937600fa224f35e87753d4d6dba3824bd9e29
Chris J. Karr <chris@audacious-software.com>
2020-08-31 22:43:17 +0000

    Initial automated code conversion

--- scripts/django_check_cpu_load.py
+++ scripts/django_check_cpu_load.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
+from builtins import str  # pylint: disable=redefined-builtin
+
 import argparse
 import json
 import sys
-import urllib2
+
+import urllib.request, urllib.error, urllib.parse
 
 CRITICAL = 90
 WARNING = 75
@@ -14,19 +19,19 @@ parser.add_argument('url', help='URL of remote endpoint to check.')
 
 args = parser.parse_args()
 
-response = urllib2.urlopen(args.url) # nosec
+response = urllib.request.urlopen(args.url) # nosec
 data = json.load(response)   
 
 if 'cpu_percentage' in data:
     if data['cpu_percentage'] > CRITICAL:
-        print 'CPU LOAD CRITICAL: ' + str(data['cpu_percentage']) + '%'
+        print('CPU LOAD CRITICAL: ' + str(data['cpu_percentage']) + '%')
         sys.exit(2)
     elif data['cpu_percentage'] > WARNING:
-        print 'CPU LOAD WARNING: ' + str(data['cpu_percentage']) + '%'
+        print('CPU LOAD WARNING: ' + str(data['cpu_percentage']) + '%')
         sys.exit(1)
     else:
-        print 'CPU LOAD OK: ' + str(data['cpu_percentage']) + '%'
+        print('CPU LOAD OK: ' + str(data['cpu_percentage']) + '%')
         sys.exit(0)
 else:
-    print 'CPU LOAD UNKNOWN: ' + args.url
+    print('CPU LOAD UNKNOWN: ' + args.url)
     sys.exit(3)
