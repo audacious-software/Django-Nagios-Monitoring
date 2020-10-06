f93b02359f7413bcb7177570790ac4003a14cf7b
Chris Karr <chris@audacious-software.com>
2020-10-06 12:23:06 -0500

    Update travis_urls.py

--- travis_urls.py
+++ travis_urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-import nagios_monitor
-
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^monitor/', include(nagios_monitor.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^monitor/', include('nagios_monitor.urls')),
 ]
