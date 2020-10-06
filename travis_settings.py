202165cf80ab7aeac84451be3e94bd626a45841b
Chris Karr <chris@audacious-software.com>
2020-10-06 12:27:23 -0500

    Update travis_settings.py

--- travis_settings.py
+++ travis_settings.py
@@ -34,8 +34,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
