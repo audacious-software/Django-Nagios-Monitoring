6d64bb097b568f6aeb3e23b92df0fd622e1fce0d
Chris J. Karr <chris@audacious-software.com>
2021-06-24 21:24:51 -0500

    Cleaning up print statements

---
+++ circle_settings.py
@@ -0,0 +1,95 @@
+# pylint: skip-file
+
+"""
+Settings.py for testing on Circle CI.
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'foobar' # nosec
+
+DEBUG = False
+ADMINS = [('Chris Karr', 'chris@audacious-software.com')]
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'nagios_monitor'
+)
+
+MIDDLEWARE = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'nagios_monitor.circle_urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'pdk.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'circle_test',
+        'USER':     'root',
+        'PASSWORD': '',
+        'HOST':     'localhost',
+        'PORT':     '',
+    }
+}
+
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
--- scripts/django_check_current_users.py
+++ scripts/django_check_current_users.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+from __future__ import print_function
+
+from builtins import str # pylint: disable=redefined-builtin
+
 import argparse
 import json
 import sys
-import urllib2
+
+import urllib.request, urllib.error, urllib.parse
 
 CRITICAL = 5
 WARNING = 2
@@ -14,19 +19,19 @@ parser.add_argument('url', help='URL of remote endpoint to check.')
 
 args = parser.parse_args()
 
-response = urllib2.urlopen(args.url) # nosec
+response = urllib.request.urlopen(args.url) # nosec
 data = json.load(response)   
 
 if 'count' in data:
     if data['count'] > CRITICAL:
-        print 'USERS CRITICAL: ' + str(data['count'])
+        print('USERS CRITICAL: ' + str(data['count']))
         sys.exit(2)
     elif data['count'] > WARNING:
-        print 'USERS WARNING: ' + str(data['count'])
+        print('USERS WARNING: ' + str(data['count']))
         sys.exit(1)
     else:
-        print 'USERS OK: ' + str(data['count'])
+        print('USERS OK: ' + str(data['count']))
         sys.exit(0)
 else:
-    print 'USERS UNKNOWN: ' + args.url
+    print('USERS UNKNOWN: ' + args.url)
     sys.exit(3)
