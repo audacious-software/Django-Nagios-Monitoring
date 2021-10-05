9fd59f5cb1c9dd6348f3b928669a46a69b568747
Chris J. Karr <chris@audacious-software.com>
2021-10-05 13:42:15 -0500

    Dependency and version updates

--- apps.py
+++ apps.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 # -*- coding: utf-8 -*-
 
 from django.apps import AppConfig
@@ -5,4 +6,4 @@ from django.apps import AppConfig
 class NagiosMonitorConfig(AppConfig):
     name = 'nagios_monitor'
     verbose_name = 'System Monitoring Support'
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
