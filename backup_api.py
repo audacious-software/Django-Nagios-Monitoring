b3a7da6375fd4b9f256fef76b88d6496fd8091c4
Chris J. Karr <chris@audacious-software.com>
2023-03-03 02:15:38 +0000

    Simple backup support

---
+++ backup_api.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+
+import bz2
+import datetime
+import gc
+import io
+import os
+import sys
+import tempfile
+
+from django.conf import settings
+from django.core import management
+from django.utils.text import slugify
+
+def incremental_backup(parameters):
+    to_transmit = []
+
+    # Dump full content of these models. No incremental backup here.
+
+    dumpdata_apps = (
+        'nagios_monitor.ScheduledEvent',
+    )
+
+    prefix = 'nagios_monitor_backup_' + settings.ALLOWED_HOSTS[0]
+
+    if 'start_date' in parameters:
+        prefix += '_' + parameters['start_date'].date().isoformat()
+
+    if 'end_date' in parameters:
+        prefix += '_' + (parameters['end_date'].date() - datetime.timedelta(days=1)).isoformat()
+
+    backup_staging = tempfile.gettempdir()
+
+    try:
+        backup_staging = settings.SIMPLE_BACKUP_STAGING_DESTINATION
+    except AttributeError:
+        pass
+
+    for app in dumpdata_apps:
+        print('[nagios_monitor] Backing up ' + app + '...')
+        sys.stdout.flush()
+
+        buf = io.StringIO()
+        management.call_command('dumpdata', app, stdout=buf)
+        buf.seek(0)
+
+        database_dump = buf.read()
+
+        buf = None
+
+        gc.collect()
+
+        compressed_str = bz2.compress(database_dump.encode('utf-8'))
+
+        database_dump = None
+
+        gc.collect()
+
+        filename = prefix + '_' + slugify(app) + '.json-dumpdata.bz2'
+
+        path = os.path.join(backup_staging, filename)
+
+        with io.open(path, 'wb') as fixture_file:
+            fixture_file.write(compressed_str)
+
+        to_transmit.append(path)
+
+    return to_transmit
